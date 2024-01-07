b05df5f0ebb802f9c7b8d0face45a997bad89721
nekronos <edubuntu2002@gmail.com>
2024-01-07 21:21:14 +0100

    Close buffer on term close

--- init.lua
+++ init.lua
@@ -11,3 +11,9 @@ vim.cmd("set tabstop=4")
 -- Set numbering
 vim.cmd("set number")
 
+-- Set terminal close on exit
+vim.api.nvim_create_autocmd("TermClose", {
+    callback = function()
+       vim.cmd("bdelete")
+    end
+})
