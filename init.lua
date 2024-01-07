f0cc8d87fb7b20ec63e4abeb7027fc716b6d0f1f
nekronos <edubuntu2002@gmail.com>
2024-01-07 20:47:42 +0100

    treesitter suport

--- init.lua
+++ init.lua
@@ -5,8 +5,9 @@ require("nekronos.lazy")
 vim.cmd("set undofile");
 
 -- Set tabulations correctly
-vim.cmd("set shiftwidth=4");
-vim.cmd("set tabstop=4");
+vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
  
 -- Set numbering
-vim.cmd("set number");
+vim.cmd("set number")
+
