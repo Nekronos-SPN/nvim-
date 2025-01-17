4b3e2b0fb4af2e3ad098c3177b6824920e5f0e7a
Eduardo Rodriguez <eduardo.rodriguez@ericsson.com>
2025-01-17 12:08:29 +0100

    FIX: change keybindings for buffer management
    Change-Id: I6e5b47ae4fa614e284018cf312639a866a413c23

--- lua/nekronos/keymaps.lua
+++ lua/nekronos/keymaps.lua
@@ -3,9 +3,10 @@ vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap=true}) -- Set term
 vim.api.nvim_set_keymap('n', '<C-B>', '<cmd>NvimTreeToggle<cr>', {noremap=true}) 	-- Toggle NvimTree
 vim.api.nvim_set_keymap('n', '<C-H>', 'gt', {noremap=true}) -- Previous tab
 vim.api.nvim_set_keymap('n', '<C-L>', 'gt', {noremap=true})-- Next tab
-vim.api.nvim_set_keymap('n', '<S-H>', '<cmd>bp<cr>', {noremap=true}) -- Previous tab
-vim.api.nvim_set_keymap('n', '<S-L>', '<cmd>bn<cr>', {noremap=true})-- Next tab
 vim.api.nvim_set_keymap('n', '<C-N>', '<cmd>tabe<cr>', {noremap=true}) -- Create new tab
+vim.api.nvim_set_keymap('n', '<S-J>', '<cmd>bp<cr>', {noremap=true}) -- Previous buffer
+vim.api.nvim_set_keymap('n', '<S-K>', '<cmd>bn<cr>', {noremap=true})-- Next buffer
+vim.api.nvim_set_keymap('n', '<S-X>', '<cmd>bd<cr>', {noremap=true})-- Delete buffer
 
 -- Toggle terminal
 vim.api.nvim_set_keymap('n', '<C-T>', '<cmd>ToggleTerm direction=float<cr>', {noremap=true})
