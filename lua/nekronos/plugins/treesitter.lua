a19056403e35b4b3ce8c2cd4c036d9dd633be5f2
Eduardo Rodriguez <eduardo.rodriguez@ericsson.com>
2025-01-17 13:39:14 +0100

    FIX: treesitter on startup
    Change-Id: I0444c7c0039eee81e5bb553ee136f399dc9cce16

--- lua/nekronos/plugins/treesitter.lua
+++ lua/nekronos/plugins/treesitter.lua
@@ -1,4 +1,14 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
-	highlight = { enable = true }
+	tag='v0.9.0',
+	config = function()
+		require('nvim-treesitter.configs').setup{
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+			sync_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		}
+	end,
 }
