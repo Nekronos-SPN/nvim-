64e1a7c6e056a25a590183f4b16ddf9dbbbb71ab
Eduardo Rodriguez <eduardo.rodriguez@ericsson.com>
2025-01-21 17:41:25 +0100

    FIX: replace slow telescope with faster fzf
    Change-Id: I2053d4c9f0a767f012adc476070c556c74fe58cd

--- lua/nekronos/keymaps.lua
+++ lua/nekronos/keymaps.lua
@@ -1,16 +1,13 @@
 -- Tab management
-vim.api.nvim_set_keymap('n', '<C-B>', '<cmd>NvimTreeToggle<cr>', {noremap=true}) 	-- Toggle NvimTree
+vim.api.nvim_set_keymap('n', '<C-B>', '<cmd>NvimTreeToggle<cr>', {noremap=true}) -- Toggle NvimTree
 vim.api.nvim_set_keymap('n', '<C-H>', 'gt', {noremap=true}) -- Previous tab
 vim.api.nvim_set_keymap('n', '<C-L>', 'gt', {noremap=true})-- Next tab
 vim.api.nvim_set_keymap('n', '<C-N>', '<cmd>tabe<cr>', {noremap=true}) -- Create new tab
 vim.api.nvim_set_keymap('n', '<leader>x', '<cmd>bp<bar>sp<bar>bn<bar>bd<cr>', {noremap=true}) -- Delete buffer and avoid closing window
 
--- Telescope usage
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+-- FZF usage
+vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files<cr>') -- File search
+vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua live_grep<cr>') -- Grep search
 
 -- Toggle terminal
 vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>ToggleTerm direction=float<cr>', {noremap=true})
---
+++ lua/nekronos/plugins/fzf.lua
@@ -0,0 +1,8 @@
+return {
+	"ibhagwan/fzf-lua",
+	config = function()
+		require('fzf-lua').setup{
+			"telescope",
+		}
+	end,
+}
