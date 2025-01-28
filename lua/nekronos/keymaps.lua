129cba646101cae18e07a383f776ef9cd9a78972
Eduardo Rodriguez <eduardo.rodriguez@ericsson.com>
2025-01-28 15:49:16 +0100

    NEW: keymap for FzF buffers: <leader>fb
    Change-Id: I13cf37f345c22ebd6e715dbabc478b11fcd8d41d

--- lua/nekronos/keymaps.lua
+++ lua/nekronos/keymaps.lua
@@ -1,13 +1,15 @@
--- Tab management
+-- Buffer management
 vim.api.nvim_set_keymap('n', '<C-B>', '<cmd>NvimTreeToggle<cr>', {noremap=true}) -- Toggle NvimTree
-vim.api.nvim_set_keymap('n', '<C-H>', 'gt', {noremap=true}) -- Previous tab
-vim.api.nvim_set_keymap('n', '<C-L>', 'gt', {noremap=true})-- Next tab
-vim.api.nvim_set_keymap('n', '<C-N>', '<cmd>tabe<cr>', {noremap=true}) -- Create new tab
 vim.api.nvim_set_keymap('n', '<leader>x', '<cmd>bp<bar>sp<bar>bn<bar>bd<cr>', {noremap=true}) -- Delete buffer and avoid closing window
 
 -- FZF usage
 vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files<cr>') -- File search
 vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua live_grep<cr>') -- Grep search
+vim.keymap.set('n', '<leader>fb', '<cmd>FzfLua buffers<cr>') -- Grep search
+
+-- LSP Diagnostics
+vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
 
 -- Toggle terminal
 vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>ToggleTerm direction=float<cr>', {noremap=true})
